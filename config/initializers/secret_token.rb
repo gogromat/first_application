c26a466cebba01d45b710aeef3c2ed74bdb6cfd1
Alexander Astafurov <gogromat@fastmail.fm>
2012-02-21 21:14:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApplication::Application.config.secret_token = '972279524061ff9b152fec3c83fb40efd2df666586289c7efddce3df753081c49cdac0d4972ce0fbb32da53e000ee332ef9654a187b2f247225f7da1723f822a'
